9b81caf81f5c554f5156ac5ef91505676d0c3f04
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-07 22:53:31 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -35,15 +35,27 @@
 
 def naming_system
   
-  PORTION_1 = {
-   label: "Kingdom",
-
-   sub_category: {}
-     label: "Phylum",
-
-     sub_category: {}
-       label: "Class"
-    
+  animals = {
+    label: "Kingdom",
+    sub_category: {
+      label: "Phylum",
+      sub_category: {
+        label: "Class",
+        sub_category: {
+           label: "Order",
+           sub_category: {
+             label: "Family",
+             sub_category: {
+               label: "Genus",
+               sub_category: {
+                 label: "Species",
+                 sub_category: nil
+            }
+           }
+         }
+       }
+      }
+    }
   }
   
   # Remember:
